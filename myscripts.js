fb48d83b8cb2e2a2adf3d1c3cce91dc2e6146714
jylopez <jershonlopez@gmail.com>
2016-09-19 14:35:31 -0600

    pokemon

---
+++ myscripts.js
@@ -0,0 +1,38 @@
+var audio = document.getElementById("pokesong");
+
+function playpokesong() {
+  audio.play();
+}
+
+function pausepokesong() {
+  audio.pause();
+}
+
+
+
+
+
+//grass B3E061
+//grassdark 94B365
+//grassshadwo 9FC754
+
+//plantlight B2E50E
+//plantdark 4C9E0C
+
+//greylight  F4F0ED
+//greymid D5CDCA
+//greydark  A09292
+//bluegreylight B6C6C5
+//bluegreydark  9EACAD
+
+//bluelight 2CD7AD
+//bluedark 24BFA0
+
+//yellow FDC33E
+//orange EE901F
+//brown B68F58
+//browndark 987543
+//red F67F7B
+//reddark AB5B66
+
+//road DDE3E3
